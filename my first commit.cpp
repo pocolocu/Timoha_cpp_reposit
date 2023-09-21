020ba709da67f763d3c3ee34addd67d4e9d57f72
pocolocu <125911684+pocolocu@users.noreply.github.com>
2023-09-21 22:16:56 +0300

    Update my first commit.cpp

--- my first commit.cpp
+++ my first commit.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-setlocale(LC_ALL, "RU"); //создаю переменную int и меняю её значение
+setlocale(LC_ALL, "RU"); //локализатор
   int num = 2;
-  num = 22;
+  num = 22; //создаю переменную int и меняю её значение
     bool NonZero = (num != 0);
         if (NonZero) {
-            cout << "num is not zero" << endl; 
+            cout << "num is not zero" << endl; //проверяю переменную на bool
     } else {
             cout << "num is zero!" << endl;
     }
